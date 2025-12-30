758d581cee0263cab1f53c316a689c702d931466
ll0zlk <hyojin041025@gmail.com>
2025-12-30 14:43:24 +0900

    add vercel analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
+  <Analytics />
+  
   <MobileWrapper>
     <router-view />
   </MobileWrapper>
 </template>
 
 <script setup>
+import { Analytics } from '@vercel/analytics/vue';
 import MobileWrapper from './components/MobileWrapper.vue'
 </script>
