dc4f12bc33e47af3fac30da81bc1aec57188dbe9
ll0zlk <hyojin041025@gmail.com>
2026-01-10 21:53:50 +0900

    add wakeup signal

--- src/App.vue
+++ src/App.vue
@@ -9,4 +9,14 @@
 <script setup>
 import { Analytics } from '@vercel/analytics/vue';
 import MobileWrapper from './components/MobileWrapper.vue'
+
+import { onMounted } from 'vue';
+import axios from 'axios';
+
+onMounted(() => {
+  axios.get('https://is-that-my-name.onrender.com/api/saju/health')
+    .catch(() => {
+      console.log("Wake-up signal sent to server.");
+    });
+});
 </script>
