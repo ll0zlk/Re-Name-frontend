4823c72a99ebac4c79f5209110975530fad5512f
ll0zlk <hyojin041025@gmail.com>
2026-01-10 23:11:29 +0900

    add log

--- src/views/ResultView.vue
+++ src/views/ResultView.vue
@@ -83,6 +83,7 @@ import html2canvas from 'html2canvas';
 const nameInfo = ref(null);
 
 onMounted(() => {
+    console.log("test")
     if (history.state && history.state.resultData) {
         const data = history.state.resultData;
         console.log(data.nameInfo.name);
